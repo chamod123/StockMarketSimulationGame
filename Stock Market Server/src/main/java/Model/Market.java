1c62cf42fedcd5103d0a0c338fe915b7ff02fb4a
Chamod Wijesena <chamodwijesena77@gmail.com>
2019-07-16 17:21:59 +0530

    Create MarketService and Event Model

--- Stock Market Server/src/main/java/Model/Market.java
+++ Stock Market Server/src/main/java/Model/Market.java
@@ -1,4 +1,16 @@
 package Model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class Market {
+
+    public ArrayList<Stock> stocks;
+    public ArrayList<Integer> marketTrends;
+    public ArrayList<Event> eventList;
+    public int turns=20;
+    private int currentTurn=0;
+
 }
