78f35178009b9cd215a6ab9544f79adffeee02ac
Melanie Wijesooriya <melaniewijesooriya@gmail.com>
2019-07-30 07:59:48 +0530

    Added  APIs to server js

--- FrontEnd/stock-market-simulator-web/src/server/server.js
+++ FrontEnd/stock-market-simulator-web/src/server/server.js
@@ -1,5 +1,5 @@
 function getStocks() {
-    console.log("called  getStocks")
+    console.log("called  getStocks")// Remove when API is integrated
     return  new Promise((resolve, reject) => { 
         fetch('https://api.myjson.com/bins/vntih')
             .then(function (response) {
@@ -39,7 +39,7 @@ function getLeaderBoard() {
 
 function getTransactioHistory() {
     console.log("called")
-    return  new Promise((resolve, reject) => { 
+    return  new Promise((resolve, reject) => {
         fetch('https://api.myjson.com/bins/1cwg6l')
             .then(function (response) {
                 return response.json();
@@ -54,6 +54,21 @@ function getPlayers() {
     console.log("called getPlayers")
     return new Promise((resolve, reject) => {
         fetch('http://localhost:8081/players/1')
+        // fetch('https://api.myjson.com/bins/1azzr5') //test myjson
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            })
+    })
+}
+
+function getPlayer(id) {
+    console.log("called getPlayers")
+    return new Promise((resolve, reject) => {
+        fetch(`http://localhost:8081/players/${id}`)
         // fetch('https://api.myjson.com/bins/1azzr5') //test myjson
             .then(function (response) {
                 console.log(response)
@@ -83,12 +98,127 @@ function postPlayers(data) {
             })
     })
 }
+
+// GET - get a stock Data
+function getStockById(id) {
+    console.log("called getPlayers")
+    return new Promise((resolve, reject) => {
+        fetch(`http://localhost:8081/stock/${id}`)
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            })
+    })
+}
+
+// GET - get stocks Data by sector
+function getStocksBySector(id) {
+    console.log("called getPlayers")
+    return new Promise((resolve, reject) => {
+        fetch(`http://localhost:8081/stockBySector/${id}`)
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            })
+    })
+}
+
+// GET - get all transaction Data
+function getTransactionHistory() {
+    console.log("called")
+    return  new Promise((resolve, reject) => { 
+        fetch('http://localhost:8081/transactions')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            });
+    })
+}
+
+// GET - get Bank Balance for a player
+function getBankBalance(id) {
+    console.log("called")
+    return  new Promise((resolve, reject) => { 
+        fetch(`http://localhost:8081/bankBalance${id}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            });
+    })
+}
+
+// GET - get getPortofolio
+function getPortofolio(id) {
+    console.log("called")
+    return  new Promise((resolve, reject) => { 
+        fetch(`http://localhost:8081/portofolio${id}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            });
+    })
+}
+
+// GET - start game
+function start() {
+    console.log("called start")
+    return  new Promise((resolve, reject) => { 
+        fetch(`http://localhost:8081/start`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            });
+    })
+}
+
+// GET - nextTurn
+function nextTurn() {
+    console.log("called nextTurn")
+    return  new Promise((resolve, reject) => { 
+        fetch(`http://localhost:8081/nextTurn`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            });
+    })
+}
+
+// GET - winner
+function winner() {
+    console.log("called winner")
+    return  new Promise((resolve, reject) => { 
+        fetch(`http://localhost:8081/winner`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                resolve(myJson)
+            });
+    })
+}
 	
 
 export {
     getStocks,
     getMyStocks,
     getPlayers,
+    getPlayer,
     getLeaderBoard,
 	postPlayers,
     getTransactioHistory
