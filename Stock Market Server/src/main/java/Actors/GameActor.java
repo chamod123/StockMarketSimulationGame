651cc4b7905201efb4b4059a667a23c41c9b649f
Chamod Wijesena <chamodwijesena77@gmail.com>
2019-07-31 14:55:18 +0530

    add Game Actor

---
+++ Stock Market Server/src/main/java/Actors/GameActor.java
@@ -0,0 +1,10 @@
+package Actors;
+
+import akka.actor.AbstractActor;
+
+public class GameActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+}
